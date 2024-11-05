c52e6b69bd511c03a570064fe8e00db68ddce03d
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-11-05 16:01:12 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,34 @@
 "use client";
 import localFont from "next/font/local";
+import { useEffect } from "react";
 import "./globals.css";
-import gsap from "gsap";
-import { usePathname } from "next/navigation";
-import {  ScrollTrigger,ScrollSmoother } from "gsap/all";
-import { useGSAP } from "@gsap/react";
 
-// Load Roboto font
 const Roboto = localFont({
   src: "./fonts/Roboto-Regular.ttf",
   variable: "--font-roboto",
   weight: "100 900",
 });
 
-// Register GSAP plugins
-if (typeof window !== "undefined") {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-}
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
+  useEffect(() => {
+    const handleScroll = () => {
+    };
 
-  useGSAP(() => {
-    ScrollSmoother.create({
-      smooth: 2,
-      effects: true,
-    });
-  }, [pathname]);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" type="image/png" sizes="any" />
         <title>Yeshwanth | Front End Developer</title>
-        <meta name="google-adsense-account" content="ca-pub-5761377407460431" />
       </head>
       <body className={`${Roboto.variable} antialiased`}>
         <div id="smooth-wrapper">
--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -27,17 +27,7 @@ export default function EnhancedPortfolio() {
     };
   }, []);
 
-  const handleScroll = (e: any) => {
-    e.preventDefault();
-    const targetId = e.currentTarget.getAttribute("href").substring(1);
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: "smooth",
-      });
-    }
-  };
+ 
 
   return (
     <div
